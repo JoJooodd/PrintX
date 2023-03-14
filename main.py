67357f15c943021b9cc6dd4ef16f2407105bc18f
Sebsssss <95044258+Seb-ST@users.noreply.github.com>
2023-03-14 09:48:30 -0600

    Inciso #2
    Se creo el codigo del inciso #2 de la serie #8.

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ residuo = 0
 # Josué Bautista
 
 # Sebastian Siquina
+residuo = mayor % 2
+
+if residuo == 0:
+    print(f'El numero {mayor} es par porque su residuo es:', residuo)
+else:
+    print(f'El numero {mayor} impar porque su residuo es:', residuo)
 
 # Eddy Poroj
